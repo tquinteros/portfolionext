3942d61c5d10278636a70df538aede9eaeaef4e5
Tomas Quinteros <109175498+tquinteros@users.noreply.github.com>
2024-01-15 01:25:02 -0300

    fixed style

--- src/components/ChatFloat/ChatFloat.tsx
+++ src/components/ChatFloat/ChatFloat.tsx
@@ -7,7 +7,8 @@ import { useState, useEffect, useRef, RefObject } from "react";
 import Input from "../Input/Input";
 
 export default function ChatFloat() {
-  const [showChat, setShowChat] = useState(false);
+  const [showChat, setShowChat] = useState(true);
+  const [showAlert, setShowAlert] = useState(true);
   const { messages, input, handleInputChange, handleSubmit, isLoading } =
     useChat();
 
@@ -28,9 +29,47 @@ export default function ChatFloat() {
     }
   }, [messages]);
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowAlert(false);
+    }, 5000);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  function insertSpaces(text: string, maxLength: number) {
+    let result = '';
+    let currentLength = 0;
+  
+    for (let i = 0; i < text.length; i++) {
+      result += text[i];
+      currentLength++;
+  
+      if (currentLength >= maxLength && i < text.length - 1) {
+        result += ' ';
+        currentLength = 0;
+      }
+    }
+  
+    return result;
+  }
+
   return (
     <div className="fixed bottom-4 right-4">
-      <Image onClick={handleOpenChat} src="/me.jpeg" width={48} height={48} alt="Me" className={`rounded-full cursor-pointer hover:opacity-75 duration-300 ${showChat ? "hidden" : "block"}`} />
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="relative">
+        {/* <motion.span
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 10 }}
+          transition={{ duration: 1, delay: 2.5 }}
+          className={`mb-4 absolute -top-16 w-32 right-1/2 -translate-x-1/2 ${!showAlert ? "block" : "hidden"}`}
+        >Talk with me</motion.span> */}
+        <Image onClick={handleOpenChat} src="/me.jpeg" width={48} height={48} alt="Me" className={`rounded-full cursor-pointer hover:opacity-75 duration-300 ${showChat ? "hidden" : "block"}`} />
+      </motion.div>
       <motion.form
         // ref={chat}
         initial={{ opacity: 0 }}
@@ -41,7 +80,7 @@ export default function ChatFloat() {
           setShowChat(true);
           handleSubmit(event);
         }}
-        className={`${showChat ? "block" : "hidden"} bg-[#050816] border rounded-md p-6`}>
+        className={`${showChat ? "block" : "hidden"} bg-[#050816] border rounded-md p-6 max-w-sm`}>
         <div onClick={handleCloseChat} className="flex justify-end mb-4 w-full cursor-pointer hover:opacity-75 duration-300">
           <span className="text-2xl">X</span>
         </div>
@@ -51,35 +90,46 @@ export default function ChatFloat() {
           className="text-white max-h-96 h-full overflow-y-auto">
           <div
             className={`flex flex-col mb-2 p-2 rounded-lg
-                  self-end items-end bg-gray-800
+                  self-start items-start bg-gray-800
                   `}
           >
-            <span
-              className={`text-xs text-right
-                  `}
-            >
-              Tomás Quinteros
-            </span>{" "}
+            <div className="flex items-center mb-2 gap-2">
+              <Image src="/me.jpeg" width={24} height={24} alt="Me" className={`rounded-full cursor-pointer hover:opacity-75 duration-300`} />
+              <span
+                className={`text-xs text-right
+                `}
+              >
+                Tomás Quinteros
+              </span>{" "}
+            </div>
             Ask me anything
           </div>
           {messages.map((m) => (
             <div
               key={m.id}
-              className={`flex flex-col mb-2 p-2 rounded-lg ${m.role === "assistant"
-                ? "self-end items-end bg-gray-800"
-                : "self-start bg-blue-700"
+              className={`flex flex-col mb-2 p-2 whitespace-normal overflow-wrap-break-word rounded-lg ${m.role === "assistant"
+                  ? "self-start items-start bg-gray-800"
+                  : "self-end items-end bg-blue-700"
                 }`}
             >
               <span
-                className={`text-xs ${m.role === "assistant" ? "text-right" : "text-left"
+                className={`text-xs ${m.role === "assistant"
+                    ? "text-left flex items-center gap-2 mb-2"
+                    : "text-right mb-2"
                   }`}
               >
                 {/* {m.role} */}
-                {
-                  m.role === 'assistant' ? "Tomás Quinteros" : "user"
-                }
+                <Image
+                  src="/me.jpeg"
+                  width={24}
+                  height={24}
+                  alt="Me"
+                  className={`${m.role === "user" ? "hidden" : "block"
+                    } rounded-full cursor-pointer hover:opacity-75 duration-300`}
+                />
+                {m.role === "assistant" ? "Tomás Quinteros" : "USER"}
               </span>{" "}
-              {m.content}
+              {insertSpaces(m.content, 25)}
             </div>
           ))}
         </div>
@@ -120,7 +170,7 @@ export default function ChatFloat() {
             <span className={`absolute w-0 h-0 transition-all duration-500 ease-out bg-[#9999ff] rounded-full group-hover:w-full group-hover:h-56`}></span>
             <span className="absolute inset-0 w-full h-full -mt-1 rounded-2xl opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
             <span className="relative">{
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#ffffff" d="M3 20v-6l8-2l-8-2V4l19 8z"/></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#ffffff" d="M3 20v-6l8-2l-8-2V4l19 8z" /></svg>
             }</span>
           </button>
         </div>
