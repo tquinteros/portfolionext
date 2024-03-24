16485e0b2865e5f23f30d551c4727b832a53d5ff
Tomas Quinteros <tomyquinteros494@gmail.com>
2024-03-23 21:27:25 -0300

    fixed_marquee

--- src/components/MarqueeTechs/MarqueeTechs.tsx
+++ src/components/MarqueeTechs/MarqueeTechs.tsx
@@ -11,7 +11,21 @@ export const MarqueeTechs = () => {
         <div key={index} className="flex mx-8 md:mx-16 flex-col justify-center items-center gap-3">
           <Image width={96} height={96} className="hidden md:block" src={item.icon} alt={item.label} />
           <Image width={64} height={64} className="block md:hidden" src={item.icon} alt={item.label} />
-          <p>{item.label}</p>
+          <p
+          className="text-lg"
+          style={{
+            textShadow:
+            "-1px -1px 0 #3f3f3f, " +
+            "-1px 0 0 #3f3f3f, " +
+            "-1px 1px 0 #3f3f3f, " +
+            "0 -1px 0 #3f3f3f, " +
+            "0 0 0 #3f3f3f, " +
+            "0 1px 0 #3f3f3f, " +
+            "1px -1px 0 #3f3f3f, " +
+            "1px 0 0 #3f3f3f, " +
+            "1px 1px 0 #3f3f3f",
+          }}
+          >{item.label}</p>
         </div>
       ))}
     </Marquee>
