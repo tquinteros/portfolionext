1c840eab0bee1b2f12287617a51a68f4c57be4a8
Tomas Quinteros <tomyquinteros494@gmail.com>
2024-03-24 21:03:53 -0300

    styles

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,14 +2,13 @@ import { projects } from "@/data/projects";
 import React from "react";
 import { ProjectsCard } from "../ProjectsCards/ProjectsCards";
 
-
 export const Projects = () => {
   return (
     <div id="projects">
       <h1 className="text-6xl text-center mt-32">Projects</h1>
       <p className="text-center mt-8 p-4 mb-4">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias libero
-        tempora reiciendis voluptatibus! Incidunt, in?
+        These are a few of the projects I&apos;ve created. If you&apos;d like to see more,
+        feel free to get in touch with me.
       </p>
       <div className="container p-4 mt-8 mx-auto grid grid-cols-12 md:gap-12 gap-8">
         {projects.map((project) => (
