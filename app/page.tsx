909dce0964e354c3a68f4146e6b57615d3acaae1
Tomas Quinteros <tomyquinteros494@gmail.com>
2024-03-25 18:42:29 -0300

    loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Chat from "@/src/components/Chat/Chat";
 import Contact from "@/src/components/Contact/Contact";
 import { Header } from "@/src/components/Header/Header";
@@ -5,19 +6,39 @@ import Hero from "@/src/components/Hero/Hero";
 import { MarqueeTechs } from "@/src/components/MarqueeTechs/MarqueeTechs";
 import { Projects } from "@/src/components/Projects/Projects";
 import Image from "next/image";
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import React, { useState, useEffect } from "react";
+import Loader from "@/src/components/Loader/Loader";
 
 export default function Home() {
+  const [pageLoaded, setPageLoaded] = useState(false);
+
+  useEffect(() => {
+    const simulatePageLoad = () => {
+      setTimeout(() => {
+        setPageLoaded(true);
+      }, 2000);
+    };
+
+    simulatePageLoad();
+  }, []);
+
   return (
     <main className="overflow-x-hidden scroll-smooth">
-      <Header />
-      <Hero />
-      <MarqueeTechs />
-      <Projects />
-      <Contact />
-      <Chat />
-      <ToastContainer />
+      {pageLoaded && <Loader />}
+
+      {!pageLoaded && (
+        <>
+          <Header />
+          <Hero />
+          <MarqueeTechs />
+          <Projects />
+          <Contact />
+          <Chat />
+          <ToastContainer />
+        </>
+      )}
     </main>
   );
 }
---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+
+const Loader = () => {
+  const [loadingProgress, setLoadingProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLoadingProgress(prevProgress => {
+        const newProgress = prevProgress + (100 / 120);
+        return newProgress >= 100 ? 100 : newProgress;
+      });
+    }, 16.67);
+
+    setTimeout(() => {
+      clearInterval(interval);
+      setLoadingProgress(100);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <p className="mt-4 text-lg font-bold">Cargando...</p>
+      <div className="w-[50%] h-4 bg-gray-200 rounded-full mt-2">
+        <div className="h-full bg-blue-500 rounded-full" style={{ width: `${loadingProgress}%` }}></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
