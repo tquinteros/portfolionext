e3a20260bed942754793fb700a580661cb304f9f
Tomas Quinteros <tomyquinteros494@gmail.com>
2024-03-26 12:29:14 -0300

    test

--- src/components/Hero/TestParticles.tsx
+++ src/components/Hero/TestParticles.tsx
@@ -20,7 +20,7 @@ export const TestParticles = () => {
   }, []);
 
   const particlesLoaded = async (container?: Container): Promise<void> => {
-    // console.log(container);
+    console.log(container);
   };
 
   const options: ISourceOptions = useMemo(
