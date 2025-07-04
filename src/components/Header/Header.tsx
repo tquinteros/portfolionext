e59adc88aeeea8a7ee473300144078bdebc61099
Tomas Quinteros <109175498+tquinteros@users.noreply.github.com>
2025-07-04 00:10:35 -0300

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -138,9 +138,9 @@ export const Header = () => {
         </motion.ul>
         {/* MOBILE */}
         <motion.div
-          initial={{ opacity: 0, y: -100 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1.2, delay: 0.3 }}
+          // initial={{ opacity: 0, y: -100 }}
+          // animate={{ opacity: 1, y: 0 }}
+          // transition={{ duration: 1.2, delay: 0.3 }}
           className="block md:hidden"
         >
           <MenuHamburger
