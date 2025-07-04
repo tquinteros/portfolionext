50ea965e7a12c3922fe63c8cb6ac096ad11fce6d
Tomas Quinteros <109175498+tquinteros@users.noreply.github.com>
2025-07-04 00:48:04 -0300

    p

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -10,7 +10,7 @@ export const Projects = () => {
       <h1 className="text-4xl font-bold tracking-tighter text-center sm:text-5xl md:text-6xl lg:text-7xl/none">
         <span className="green-text-gradient">Projects</span>
       </h1>
-      <p className="max-w-[900px] text-gray-300 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed mx-auto text-center mt-4 mb-8">
+      <p className="max-w-[900px] text-gray-300 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed mx-auto text-center mt-4 mb-8 p-2 md:p-0">
         These are a few of the projects I&apos;ve created. If you&apos;d like to see more, feel free to get in touch
         with me.
       </p>
